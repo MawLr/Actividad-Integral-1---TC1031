3ab88b87e8a744c9d682d679df9cb58c4bdaa3dc
MawLr <58478264+MawLr@users.noreply.github.com>
2020-12-02 21:06:13 -0600

    Correcciones y adición de casos de prueba

--- Actividad Integral 4/main.cpp
+++ Actividad Integral 4/main.cpp
@@ -1,27 +1,28 @@
-//
-//  main.cpp
-//  proyecto 1 tc1031
-//
-//  AVANCE 4:
-//  A01706212
-//  Created by Manolo Medina on 9/16/20. A0170212
-//  Copyright © 2020 Manolo Medina. All rights reserved.
-//
-
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <vector>
 #include <string>
+#include <cstring>
 #include "graph.h"
 
 using namespace std;
 
 int main() {
   Graph g;
+  Graph test;
+
+  string gtest_ans;
+
   ifstream inFile;
   ofstream output;
   bool fileCheck = false;
+  string cOrigen;
+  string cDestino;
+  int precioO;
+  int precioD;
+  int precioR;
+
   cout << "Bienvenido a este avance de grafos, por favor selecciona una opción" << endl;
 
   bool exit = false;
@@ -34,14 +35,16 @@ int main() {
     int initV;
     int targetV;
     
-    cout << "(1) Leer archivo de grafos..." << endl;
-    cout << "(2) Imprimir lista adyacente..." << endl;
-    cout << "(3) Imprimir matriz adyacente..." << endl;
-    cout << "(4) Realizar búsqueda en profundidad..." << endl;
-    cout << "(5) Realizar búsqueda en anchura..." << endl;
-    cout << "(6) Realizar ejemplo de uso de grafos..." << endl;
-    cout << "(7) Generar archivo con lista y matriz adyacente..." << endl;
-    cout << "(8) Salir..." << endl;
+    
+    cout << "(1) Leer archivo de RUTAS: " << endl;
+    cout << "(2) Imprimir lista adyacente: " << endl;
+    cout << "(3) Imprimir matriz adyacente: " << endl;
+    cout << "(4) Desplegar Rutas: " << endl;
+    cout << "(5) Buscar Ruta Rápida: " << endl;
+    cout << "(6) Buscar Ruta Económica: " << endl;
+    cout << "(7) Casos de Prueba: " << endl;
+    cout << "(8) Generar archivo con lista y matriz adyacente: " << endl;
+    cout << "(9) Salir" << endl;
     cin >> option;
     if (option == 1){
       fileCheck = true;
@@ -49,14 +52,15 @@ int main() {
     switch(option){
       
       case 1:
-      cout << "Ingrese nombre de archivo de grafos: " << endl;
-      cin >> fileGraph;
-      cout << "Ingrese número de vertices: " << endl;
-      cin >> vNum;
-      cout << "Ingrese número de edges: " << endl;
-      cin >> aNum;
+      cout << "Leyendo archivo de grafos..." << endl;
+      fileGraph = "rutas.txt";
+      cout << "Leyendo número de vertices: " << endl;
+      vNum = 8;
+      cout << "Leyendo número de edges: " << endl;
+      aNum = 8;
       g.loadGraphList(fileGraph, vNum, aNum); // Método para cargar grafo
       g.loadGraphMat(fileGraph, vNum, aNum);
+      cout << '\n';
       
       break;
 
@@ -79,16 +83,14 @@ int main() {
       break;
 
       case 4:
-      if (fileCheck == false){
-        cout << "Se requiere un archivo para poder ejecutar esta opción" << endl;
-        cout << '\n';
-        break;
-      }
-      cout << "Ingrese vertice de inicio"<< endl;
-      cin >> initV;
-      cout << "Ingrese vertice objetivo"<< endl;
-      cin >> targetV;
-      cout << g.DFS(initV, targetV) << endl;
+      cout << "RUTAS:" << endl;
+      cout << "  QRO:      0" << endl;
+      cout << "  SLP:      2" << endl;
+      cout << "  ZAC:      4" << endl;
+      cout << "  VER:      1" << endl;
+      cout << "  TMP:      6" << endl;
+      cout << "  MTY:      7" << endl;
+      cout << '\n';
       break;
 
       case 5:
@@ -97,45 +99,193 @@ int main() {
         cout << '\n';
         break;
       }
-      cout << "Ingrese vertice de inicio"<< endl;
-      cin >> initV;
-      cout << "Ingrese vertice objetivo"<< endl;
-      cin >> targetV;
-      cout << g.BFS(initV, targetV) << endl;
+      cout << "Ingrese ciudad de origen"<< endl;
+      cin >> cOrigen;
+      if (cOrigen == "QRO" || cOrigen == "qro"){
+        initV = 0;
+        precioO = 250;
+      }
+      else if(cOrigen == "SLP" || cOrigen == "slp"){
+        initV = 2;
+        precioO = 210;
+      }
+      else if(cOrigen == "ZAC" || cOrigen == "zac"){
+        initV = 4;
+        precioO = 200;
+      }
+      else if(cOrigen == "VER" || cOrigen == "ver"){
+        initV = 1;
+        precioO = 220;
+      }
+      else if (cDestino == "TMP" || cDestino == "tmp"){
+        targetV = 6;
+        precioD = 220;
+      }
+      else if(cOrigen == "MTY" || cOrigen == "mty"){
+        initV = 7;
+        precioO = 330;
+      }
+      else{
+        cout << "Ingrese orígen válido de la tabla de rutas..." << endl;
+        break;
+      }
+
+      cout << "Ingrese ciudad de destino"<< endl;
+      cin >> cDestino;
+
+      if (cDestino == "QRO" || cDestino == "qro"){
+        targetV = 0;
+        precioD = 250;
+      }
+      else if (cDestino == "SLP" || cDestino == "slp"){
+        targetV = 2;
+        precioD = 210;
+      }
+      else if (cDestino == "ZAC" || cDestino == "zac"){
+        targetV = 4;
+        precioD = 200;
+      }
+      else if (cDestino == "VER" || cDestino == "ver"){
+        targetV = 1;
+        precioD = 220;
+      }
+      else if (cDestino == "TMP" || cDestino == "tmp"){
+        targetV = 6;
+        precioD = 220;
+      }
+      else if (cDestino == "MTY" || cDestino == "mty"){
+        targetV = 7;
+        precioD = 330;
+      }
+      else{
+        cout << "Ingrese destino válido de la tabla de rutas..." << endl;
+        break;
+      }
+
+      if(precioD == precioO){
+        precioR = 0;
+        cout << "No puedes viajar a donde ya estás"<< endl;
+        break;
+      }
+      else{
+        precioR = precioO + precioD;
+      }
+      
+      cout << "Precio por esta ruta: $" << precioR << endl;
+      cout << g.DFS(initV, targetV) << endl;
+      cout << '\n';
       break;
 
       case 6:
-
       if (fileCheck == false){
-        cout << "Para este ejemplo se requiere el archivo «t1.txt», favor de leerlo antes de correr esta opción" << endl;
+        cout << "Se requiere un archivo para poder ejecutar esta opción" << endl;
         cout << '\n';
         break;
       }
-      cout << "Vamos a realizar un ejemplo de uso de grafos, en este caso nos vamos a imaginar rutas de autobus... \n" << endl;
-      cout << "En este ejemplo, los grafos nos van a ayudar a poder llegar en la rutas más rápida y la menos costosa, haciendo uso de las búsquedas por anchura y por profundidad... \n" << endl;
+      cout << "Ingrese ciudad de origen"<< endl;
+      cin >> cOrigen;
+      if (cOrigen == "QRO" || cOrigen == "qro"){
+        initV = 0;
+        precioO = 100;
+      }
+      else if(cOrigen == "SLP" || cOrigen == "slp"){
+        initV = 2;
+        precioO = 110;
+      }
+      else if(cOrigen == "ZAC" || cOrigen == "zac"){
+        initV = 4;
+        precioO = 120;
+      }
+      else if(cOrigen == "VER" || cOrigen == "ver"){
+        initV = 1;
+        precioO = 130;
+      }
+      else if (cDestino == "TMP" || cDestino == "tmp"){
+        targetV = 6;
+        precioD = 140;
+      }
+      else if(cOrigen == "MTY" || cOrigen == "mty"){
+        initV = 7;
+        precioO = 150;
+      }
+      else{
+        cout << "Ingrese orígen válido de la tabla de rutas..." << endl;
+        break;
+      }
+      
+      cout << "Ingrese ciudad de destino"<< endl;
+      cin >> cDestino;
 
-      cout << "Suponiendo que el vertice de inicio es Querétaro y el objetivo es Monterrey, hagamos la prueba: \n" << endl;
+      if (cDestino == "QRO" || cDestino == "qro"){
+        targetV = 0;
+        precioD = 100;
+      }
+      else if (cDestino == "SLP" || cDestino == "slp"){
+        targetV = 2;
+        precioD = 110;
+      }
+      else if (cDestino == "ZAC" || cDestino == "zac"){
+        targetV = 4;
+        precioD = 120;
+      }
+      else if (cDestino == "VER" || cDestino == "ver"){
+        targetV = 1;
+        precioD = 130;
+      }
+      else if (cDestino == "TMP" || cDestino == "tmp"){
+        targetV = 6;
+        precioD = 140;
+      }
+      else if (cDestino == "MTY" || cDestino == "mty"){
+        targetV = 7;
+        precioD = 150;
+      }
+      else{
+        cout << "Ingrese destino válido de la tabla de rutas..." << endl;
+        break;
+      }
 
-      cout << "QUERÉTARO:      0" << endl;
-      cout << "SLP:            2" << endl;
-      cout << "ZACATECAS:      4" << endl;
-      cout << "MATEHUALA:      1" << endl;
-      cout << "PIEDRAS NEGRAS: 6" << endl;
-      cout << "MONTERREY:      7" << endl;
-      cout << '\n';
-      cout << "Precio por esta ruta: 450$" << endl;
-      cout << g.BFS(0, 7) << endl;
-      cout << '\n';
-      cout << "Precio por esta ruta: 575$" << endl;
-      cout << g.DFS(0, 7) << endl;
-      cout << '\n';
-      cout << "Como podemos ver, la búsqueda de profundidad nos arrojó la ruta más rápida con nuestros nodos entre las ciudades hipotéticas, pero esto depende de las rutas conectadas, de esta forma podemos crear rutas y cobrar diferentes precios por rapidez, etc." << endl;;
-      cout << '\n';
-      cout << "Igualmente este es un caso de prueba, demostrando que el programa funciona" << endl;
+      if(precioD == precioO){
+        precioR = 0;
+        cout << "No puedes viajar a donde ya estás"<< endl;
+        break;
+      }
+      else{
+        precioR = precioO + precioD;
+      }
+      
+      cout << "Precio por esta ruta: $" << precioR << endl;
+      cout << g.BFS(initV, targetV) << endl;
       cout << '\n';
       break;
 
       case 7:
+
+      if (fileCheck == false){
+        cout << "Se requiere un archivo para poder ejecutar esta opción" << endl;
+        cout << '\n';
+        break;
+      }
+
+      gtest_ans = "vertex 0 : 2 7 vertex 1 : 5 6 vertex 2 : 0 4 vertex 3 : vertex 4 : 2 5 vertex 5 : 1 4 vertex 6 : 1 7 vertex 7 : 0 6 ";
+
+      cout << " 1 " <<	(!gtest_ans.compare(g.printAdjList()) ? "success\n" : "fail\n");
+
+      gtest_ans = "0 0 1 0 0 0 0 1 0 0 0 0 0 1 1 0 1 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 1 0 0 0 1 0 0 1 0 0 0 0 1 0 0 0 0 0 1 1 0 0 0 0 0 1 0 ";
+
+      cout << " 2 " <<	(!gtest_ans.compare(g.printAdjMat()) ? "success\n" : "fail\n");
+
+      gtest_ans = "visited: 0 7 path: 0 7";
+
+      cout << " 3 " <<	( (!gtest_ans.compare(g.DFS(0,7))) ? "success\n" : "fail\n");
+
+      gtest_ans = "visited: 0 2 7 path: 0 7";
+
+      cout << " 4 " <<	( (!gtest_ans.compare(g.BFS(0,7))) ? "success\n" : "fail\n");
+      
+      break;
+
+      case 8:
       if (fileCheck == false){
         cout << "Se requiere un archivo para poder ejecutar esta opción" << endl;
         cout << '\n';
@@ -153,7 +303,7 @@ int main() {
       output.close();
       break;
 
-      case 8:
+      case 9:
       cout << "FIN DE SESIÓN" << endl;
       exit = true;
     }
